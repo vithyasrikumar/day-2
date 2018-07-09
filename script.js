e24a30846a44b894cdfbb381913c5f021ac63fb6
Sean Catangui <seancaat@gmail.com>
2018-07-09 10:06:06 -0400

    adding exercises

---
+++ script.js
@@ -0,0 +1,64 @@
+window.onload = function () {
+	
+	document.querySelector('.puzzle-0').addEventListener('click', function() {
+	  window.alert('Hello, World!');
+	});
+
+	document.querySelector('.puzzle-1').addEventListener('click', function() {
+		
+	});
+
+	document.querySelector('.puzzle-2').addEventListener('click', function() {
+
+	});
+
+	document.querySelector('.puzzle-3').addEventListener('click', function() {
+
+	});
+
+	document.querySelector('.puzzle-4').addEventListener('click', function() {
+
+	});
+
+	document.querySelector('.puzzle-5').addEventListener('click', function() {
+
+	});
+
+	document.querySelector('.puzzle-6-yellow').addEventListener('click', function() {
+		
+	});
+
+	document.querySelector('.puzzle-6-pink').addEventListener('click', function() {
+		
+	});
+
+	document.querySelector('.puzzle-6-purple').addEventListener('click', function() {
+		
+	});
+
+	document.querySelector('.puzzle-7').addEventListener('click', function() {
+		
+	});
+
+	document.querySelector('.puzzle-8').addEventListener('click', function() {
+
+	});
+
+	// Challenge puzzles
+
+	document.querySelector('.puzzle-9')
+
+	document.querySelector('.puzzle-10')
+
+	document.querySelector('.puzzle-11')
+
+	document.querySelector('.puzzle-12')
+
+	document.querySelector('.puzzle-13')
+
+	document.querySelector('.puzzle-14')
+
+	document.querySelector('.puzzle-15')
+
+
+}
